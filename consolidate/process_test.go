db0e4ad23c2616a178c2c2eec069d31361d2948d
piux2 <90544084+piux2@users.noreply.github.com>
2022-07-09 21:16:01 -0700

    chore: update consolidation (#7)
    Co-authored-by: piux2 <>

--- consolidate/process_test.go
+++ consolidate/process_test.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"fmt"
+	"github.com/cosmos/cosmos-sdk/types"
+	osm "github.com/gnolang/gno/pkgs/os"
 	"github.com/stretchr/testify/assert"
+	"strconv"
+	"strings"
 	"testing"
 )
 
@@ -23,22 +28,126 @@ func TestConvertAddress(t *testing.T) {
 
 	ledger, err = convertAddress(ledger_address_gno)
 	assert.Error(t, err)
+
+	//TODO: test multisig convertion
+}
+
+var a1 = Account{
+	Address: "cosmos1fupfatmln5844rjafzp6d2vc825vav2xe277uu",
+	Coins: []Coin{
+		{Amount: "455794000000", Denom: "uatom"},
+		{Amount: "5083895000000", Denom: "duatom"},
+	},
+	Vote: "{\"option\":1,\"weight\":\"1.000000000000000000\"}",
+}
+
+var a2 = Account{
+	Address: "cosmos1fz9nhh7upfn9sv02f3ck4zsu8uqaesmupv6pv2",
+	Coins: []Coin{
+		{Amount: "455794000000", Denom: "uatom"},
+		{Amount: "5083895000000", Denom: "duatom"},
+	},
+	Vote: "{\"option\":3,\"weight\":\"1.000000000000000000\"}",
+}
+
+var a3 = Account{
+	Address: "cosmos1zzzyklkaqafpe8200y7y6y3u9a3cehkr223223",
+	Coins: []Coin{
+		{Amount: "1", Denom: "uatom"},
+	},
+	Vote: "",
+}
+
+func TestQualify(t *testing.T) {
+	// one
+	var accounts = []Account{a1}
+
+	dist, totalWeight := qualify(accounts)
+
+	assert.Equal(t, 455794000000, dist[0].Weight)
+	assert.Equal(t, 455794000000, totalWeight)
+
+	//two
+	accounts = append(accounts, a2)
+
+	dist, totalWeight = qualify(accounts)
+
+	assert.Equal(t, 455794000000, dist[0].Weight)
+	assert.Equal(t, 8081636500000, dist[1].Weight)
+	assert.Equal(t, 8537430500000, totalWeight)
+
 }
 
 func TestDistribute(t *testing.T) {
 
-	vote := "{\"option\":1,\"weight\":\"1.000000000000000000\"}"
-	uatoms := 5083895 + 455794
-	duatoms := 5083895
+	var accounts = []Account{a1}
 
-	// test >0.5 round up
-	assert.Equal(t, 6, distribute(vote, duatoms, uatoms))
+	dist, totalWeight := qualify(accounts)
+	dist = distribute(dist, totalWeight)
+	// get entire distribution, TOTAL_AIRDROP = 750Mgnot
+	assert.Equal(t, 455794000000, dist[0].Weight)
+	assert.Equal(t, int64(TOTAL_AIRDROP*1000000), dist[0].Ugnot.RoundInt64())
 
-	vote = "{\"option\":1,\"weight\":\"1.000000000000000000\"}"
-	uatoms = 5083895
-	duatoms = 0
+	//  a portion
 
-	// test not delegation no distribution
-	assert.Equal(t, 0, distribute(vote, duatoms, uatoms))
+	accounts = append(accounts, a2)
+	dist, totalWeight = qualify(accounts)
+	dist = distribute(dist, totalWeight)
+
+	assert.Equal(t, 455794000000, dist[0].Weight)
+	assert.Equal(t, 8081636500000, dist[1].Weight)
+	assert.Equal(t, 8537430500000, totalWeight)
+	assert.Equal(t, "40040794475574", whole(dist[0].Ugnot.String()))
+	assert.Equal(t, "709959205524425", whole(dist[1].Ugnot.String()))
+
+	// tiny portion
+	accounts = append(accounts, a3)
+	dist, totalWeight = qualify(accounts)
+	dist = distribute(dist, totalWeight)
+
+	assert.Equal(t, 455794000000, dist[0].Weight)
+	assert.Equal(t, 8081636500000, dist[1].Weight)
+	assert.Equal(t, 1, dist[2].Weight)
+	assert.Equal(t, 8537430500001, totalWeight)
+
+	assert.Equal(t, "40040794475569", whole(dist[0].Ugnot.String()))
+	assert.Equal(t, "709959205524342", whole(dist[1].Ugnot.String()))
+	assert.Equal(t, "87", whole(dist[2].Ugnot.String()))
+
+}
+func TestTotal(t *testing.T) {
+
+	bz := osm.MustReadFile("genbalance.txt")
+
+	line := strings.TrimSuffix(string(bz), "\n")
+
+	balances := strings.Split(line, "\n")
+
+	sum := types.ZeroDec()
+
+	for _, v := range balances {
+		//cosmos10008uvk6fj3ja05u092ya5sx6fn355wavael4j:g10008uvk6fj3ja05u092ya5sx6fn355walp9u5k=3204884ugnot
+		//split and drop cosmos address
+		a := strings.Split(v, ":")
+		parts := strings.Split(a[1], "=")
+		if len(parts) != 2 {
+
+			fmt.Printf("error in parsing: %v\n", parts)
+		}
+
+		amount := strings.TrimSuffix(parts[1], "ugnot")
+
+		amount_i, err := strconv.Atoi(amount)
+
+		if err != nil {
+			panic(err)
+		}
+
+		amount_dec := types.NewDec(int64(amount_i))
+		sum = sum.Add(amount_dec)
+
+	}
+
+	assert.Equal(t, "749999999663089.000000000000000000", sum.String())
 
 }
