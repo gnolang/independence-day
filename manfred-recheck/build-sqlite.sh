f9995ad08b57defc59536d0696b7b453770ff4aa
Manfred Touron <94029+moul@users.noreply.github.com>
2022-09-09 23:12:15 +0000

    chore: update skip list
    Signed-off-by: Manfred Touron <94029+moul@users.noreply.github.com>

--- manfred-recheck/build-sqlite.sh
+++ manfred-recheck/build-sqlite.sh
@@ -9,13 +9,14 @@ CREATE TABLE accounts (
        liquid_atoms DOUBLE DEFAULT 0,
        staked_atoms DOUBLE DEFAULT 0,
        unbounding_atoms DOUBLE DEFAULT 0,
-       skip_reason string STRING DEFAULT "",
+       tag STRING DEFAULT "",
+       comment STRING DEFAULT "",
        cummulative_atoms DOUBLE
 );
 CREATE TABLE uatom_holders (address STRING PRIMARY KEY, quantity DOUBLE);
 CREATE TABLE delegations_grouped (address STRING PRIMARY KEY, quantity DOUBLE, count INTEGER);
 CREATE TABLE undelegations_grouped (address STRING PRIMARY KEY, quantity DOUBLE, count INTEGER);
-CREATE TABLE skip_reasons (address STRING PRIMARY KEY, reason STRING);
+CREATE TABLE skips (address STRING PRIMARY KEY, tag STRING, reason STRING);
 .tables
 
 SELECT "----- IMPORT CSV FILES";
@@ -23,7 +24,7 @@ SELECT "----- IMPORT CSV FILES";
 .import summaries/uatom_holders.csv uatom_holders
 .import summaries/delegations_grouped.csv delegations_grouped
 .import summaries/undelegations_grouped.csv undelegations_grouped
-.import skip.csv skip_reasons
+.import skip.csv skips
 
 SELECT "----- CHECK CONTENT OF IMPORTED CSV TABLES";
 .mode column
@@ -35,8 +36,8 @@ SELECT COUNT(*) FROM undelegations_grouped;
 SELECT * FROM undelegations_grouped LIMIT 1;
 SELECT COUNT(*) FROM undelegations_grouped;
 SELECT * FROM undelegations_grouped LIMIT 1;
-SELECT COUNT(*) FROM skip_reasons;
-SELECT * FROM skip_reasons LIMIT 1;
+SELECT COUNT(*) FROM skips;
+SELECT * FROM skips LIMIT 1;
 
 SELECT "----- AGGREGATE TEMP TABLES";
 INSERT INTO accounts(address, liquid_atoms) \
@@ -49,10 +50,10 @@ INSERT INTO accounts(address, unbounding_atoms) \
        SELECT address, quantity FROM undelegations_grouped WHERE true \
        ON CONFLICT(address) DO \
        UPDATE SET staked_atoms = excluded.unbounding_atoms;
-INSERT INTO accounts(address, skip_reason) \
-       SELECT address, reason FROM skip_reasons WHERE true \
+INSERT INTO accounts(address, tag, comment) \
+       SELECT address, tag, reason FROM skips WHERE true \
        ON CONFLICT(address) DO \
-       UPDATE SET skip_reason = excluded.skip_reason;
+       UPDATE SET tag = excluded.tag, comment = excluded.comment;
 UPDATE accounts SET liquid_atoms = liquid_atoms / 1000000, staked_atoms = staked_atoms / 1000000, unbounding_atoms = unbounding_atoms / 1000000;
 UPDATE accounts SET cummulative_atoms = (liquid_atoms + staked_atoms + unbounding_atoms);
 
