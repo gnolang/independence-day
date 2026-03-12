5381e8fe98d93f63b4bc61e9f6ca617114a7116e
Nemanja Aleksic <kouteki@gmail.com>
2026-03-12 11:37:15 +0100

    Update consolidate/process_consolidated.go
    Co-authored-by: moul <94029+moul@users.noreply.github.com>

--- consolidate/process_consolidated.go
+++ consolidate/process_consolidated.go
@@ -44,7 +44,7 @@ const (
 
 	MULTISIG_NT1_ADDRESS = "g1pxj9x5jkklzam9v76q7sn7grm0xnuj69qu7lmf" //nt1: nt llc + investors
 	MULTISIG_NT2_ADDRESS      = "g1sp27hn785v3kud6cg9dnhrng7wzp9cnljffhcg" //nt2: special case handling for aib accounts
-	MULTISIG_GOVDAO_ADDRESS  = "g1rp7cmetn27eqlpjpc4vuusf8kaj746tysc0qgh" //gnoland contributors
+	MULTISIG_GOVDAO_ADDRESS  = "g1rp7cmetn27eqlpjpc4vuusf8kaj746tysc0qgh" // govdao t1
 )
 
 var ibcEscrowAddress = map[string]bool{}
